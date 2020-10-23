84119b4d4ccde1d3fe8dca783af1e7450c544118
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-10-23 11:28:12 +0900

    define GAME class

---
+++ game.py
@@ -0,0 +1,42 @@
+class GAME():
+    # init game
+    def __init__(self):
+        start_menu()
+        select_menu()
+
+    # show start menu
+    def start_menu():
+        pass
+
+    # show select menu
+    def select_menu():
+        pass
+
+    # start game
+    def start():
+        pass
+
+    # roll dice randomly
+    def roll_dice():
+        pass
+
+    # check each player's winning condition
+    def check_win_condition():
+        pass
+    
+    # check game's exit condition
+    def check_exit_condition():
+        pass
+
+    # move player by dice number
+    def move_player():
+        pass
+
+    # run some events on game field
+    # add more any events
+    def event1():
+        pass
+
+    # show result
+    def show_result():
+        pass
