b5f37fa52ac930afeab7e0fb6c370a68f2fe731d
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-10-23 11:28:28 +0900

    define PLAYER class

---
+++ player.py
@@ -0,0 +1,9 @@
+class PLAYER():
+    # init player status
+    def __init__(self):
+        self.money = 0
+        self.muscle = 0
+        self.stress = 0
+        self.job = None
+        self.dex = 0
+        self.condition = False
