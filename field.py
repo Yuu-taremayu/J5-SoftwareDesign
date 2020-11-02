0bc9744ac6a1b82b65a3f5c6c7467744e4b57652
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-11-02 21:13:47 +0900

    add field size setter

--- field.py
+++ field.py
@@ -1,4 +1,10 @@
 class FIELD():
     # init field and properties
     def __init__(self):
-        pass
+        self.x, self.y = self.set_field()
+
+    # set field size
+    def set_field(self):
+        x = int(input('input x'))
+        y = int(input('input y'))
+        return x, y
