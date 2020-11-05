139f3dafafa45d9c6f2626a112c5cc5bbf996e27
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-11-05 22:20:55 +0900

    define setting events num function

--- field.py
+++ field.py
@@ -2,12 +2,19 @@ class FIELD():
     # init field and properties
     def __init__(self):
         self.x, self.y = self.set_field()
+        self.num_shop, self.num_jobchange = set_event()
 
     # set field size
     def set_field(self):
         x = int(input('input x'))
         y = int(input('input y'))
         return x, y
+
+    # set any event num
+    def set_events(self):
+        num_shop = 4
+        num_jobchange = 2
+        return num_shop, num_jobchange
 
     # run some events on field
     # add any more events
