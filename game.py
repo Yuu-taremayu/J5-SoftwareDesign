b84849e2a52c863c18deffd864c66c1131f8d75a
satoshi53 <fukuchan.satoshi5021@gmail.com>
2020-12-03 17:36:02 +0900

    write show_result

--- game.py
+++ game.py
@@ -13,6 +13,7 @@ class GAME():
         self.MAG = mag
         self.root = root
         self.scene_cnt = 0
+        self.goal_order = []
 
         # Variables of function
         self.var_start_menu = (1, None)
@@ -250,7 +251,9 @@ class GAME():
 
     # check each player's winning condition
     def check_win_condition(self):
-        self.player[self.turn].condition = True
+        if self.player[self.turn].condition == False:
+            self.player[self.turn].condition = True
+            self.goal_order.append(self.player[self.turn].name)
 
     # check game's exit condition
     def check_exit_condition(self):
@@ -258,9 +261,11 @@ class GAME():
         for i in range(4):
             if self.player[i].condition == False:
                 flag = False
+
         if flag == True:
-            print("ゲームを終了")
-        return flag
+            print("ゲーム終了条件が整いました")
+            self.scene_cnt += 1
+            self.show_result()
 
     # move player by dice number
     def move_player(self):
@@ -304,5 +309,21 @@ class GAME():
 
     # show result
     def show_result(self):
+        # Fill black
+        canvas = tk.Canvas(bg="black", width=self.WIDTH, height=self.HEIGHT)
+        canvas.place(x=0, y=0)
+        print(self.goal_order)
 
-        pass
+        player_num = 4
+        title = tk.Label(text="結果発表", font=("Menlo", int(self.MAG/6)))
+        for i in range(player_num):
+            name = tk.Label(text=self.goal_order[i], font=("Menlo", int(self.MAG/6)))
+            order = tk.Label(text=str(i+1)+"位", font=("Menlo", int(self.MAG/6)))
+            if self.MAG <= 60:
+                name.place(x=self.WIDTH/10*5, y=self.HEIGHT/10*4+(i*30), width=100, height=25, anchor=tk.CENTER)
+                order.place(x=self.WIDTH/10*3, y=self.HEIGHT/10*4+(i*30), width=40, height=30, anchor=tk.CENTER)
+                title.place(x=self.WIDTH/10*5, y=self.HEIGHT/10*2, width=100, height=25, anchor=tk.CENTER)
+            else:
+                name.place(x=self.WIDTH/10*5, y=self.HEIGHT/10*4+(i*self.MAG/2), width=self.MAG*2, height=self.MAG/3, anchor=tk.CENTER)
+                order.place(x=self.WIDTH/10*3, y=self.HEIGHT/10*4+(i*self.MAG/2), width=self.MAG*3/2, height=self.MAG*2/5, anchor=tk.CENTER)
+                title.place(x=self.WIDTH/10*5, y=self.HEIGHT/10*2, width=self.MAG*2, height=self.MAG/3, anchor=tk.CENTER)
