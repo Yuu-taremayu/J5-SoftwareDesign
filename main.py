1dafc664b4c0181f409d6badd8181fe57731cd73
Yuu-taremayu <taremayu.yuu@gmail.com>
2020-12-31 00:25:00 +0900

    reformat with black

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ from game import GAME
 from player import PLAYER
 import tkinter
 
+
 def main():
-    #ウィンドウのインスタンス生成
+    # ウィンドウのインスタンス生成
     root = tkinter.Tk()
 
-    #ウィンドウの設定
+    # ウィンドウの設定
     root.title("prototype")
-    window_ratio = (16, 9)#(w, h)
-    #window_magnification = 120
-    window_magnification = 70#120代入で1920*1080になる
-    h = window_ratio[1]*window_magnification#ウィンドウの高さ
-    w = window_ratio[0]*window_magnification#ウィンドウの幅
+    window_ratio = (16, 9)  # (w, h)
+    window_magnification = 120
+    # window_magnification = 70#120代入で1920*1080になる
+    h = window_ratio[1] * window_magnification  # ウィンドウの高さ
+    w = window_ratio[0] * window_magnification  # ウィンドウの幅
     mag = window_magnification
 
     root.minsize(w, h)
 
-    #gameのインスタンス生成
+    # gameのインスタンス生成
     g = GAME(w, h, mag, root)
 
-    #割り込みが起きる以外はループ
+    # 割り込みが起きる以外はループ
     root.mainloop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
